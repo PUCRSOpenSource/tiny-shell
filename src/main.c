bbde8ce10d5e15435c2594bbd936155d74c7e6f0
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-11-22 02:01:30 -0200

    chamada de métodos

--- src/main.c
+++ src/main.c
@@ -217,7 +217,202 @@ void mkdir(char* path)
 
 int main(void)
 {
-	init();
+	//init();
+	
+	char name[4096];
+	fgets(name,4096,stdin);
+
+
+	//printf("%s", name);		
+	if(name[0] == 'a')
+	{	
+		//printf("%s", name);	
+		char append[7];
+		char append2[4000];
+		int i;
+		//printf("%s", name);
+		for(i=0; i < 7; ++i)
+		{
+			//printf("%s", name);	
+			append[i] = name[i];
+			//printf("Append++\n");					
+		}
+		//printf("%s", append);
+		//printf("%s", name);
+		if ( strcmp(append, "append ") == 0 && name[7] == '/')
+		{
+			//printf("%s", append);
+			for(i = 7; i < strlen(name)-1; ++i)
+			{
+				append2[i-7] = name[i];
+			}
+			//append(append2);
+			//printf("%s", name);
+			printf("ChamaAppend\n");			
+			printf("%s", append2);
+			
+        	}
+
+	}
+	else if(name[0] == 'c')
+	{	
+		char create[7];
+		char create2[4000];
+		int i;	
+		for(i=0; i < 7; ++i)
+		{
+			create[i] = name[i];					
+		}		
+		if ( strcmp(create, "create ") == 0 && name[7] == '/')
+		{		
+			for(i = 7; i < strlen(name)-1; ++i)
+			{
+				create2[i-7] = name[i];
+			}
+			//create(create2);
+			printf("ChamaCreate\n");			
+			printf("%s", create2);			
+        	}
+
+	}
+	else if(name[0] == 'i')
+	{	
+		char init[4];	
+		int i;	
+		for(i=0; i < 4; ++i)
+		{
+			init[i] = name[i];					
+		}		
+		if ( strcmp(init, "init") == 0)
+		{
+			//init(init);
+			printf("ChamaInit\n");			
+			printf("%s", init);			
+        	}
+
+	}
+	else if(name[0] == 'l' && name[1] == 'o')
+	{	
+		char load[4];	
+		int i;	
+		for(i=0; i < 4; ++i)
+		{
+			load[i] = name[i];					
+		}		
+		if ( strcmp(load, "load") == 0)
+		{
+			//load(load);
+			printf("ChamaLoad\n");			
+			printf("%s", load);			
+        	}
+
+	}
+	else if(name[0] == 'l')
+	{	
+		char ls[3];
+		char ls2[4000];
+		int i;	
+		for(i=0; i < 3; ++i)
+		{
+			ls[i] = name[i];					
+		}		
+		if ( strcmp(ls, "ls ") == 0 && name[3] == '/')
+		{		
+			for(i = 3; i < strlen(name)-1; ++i)
+			{
+				ls2[i-3] = name[i];
+			}
+			//ls(ls2);
+			printf("ChamaLs\n");			
+			printf("%s", ls2);			
+        	}
+
+	}
+	else if(name[0] == 'm')
+	{	
+		char mkdir[6];
+		char mkdir2[4000];
+		int i;	
+		for(i=0; i < 6; ++i)
+		{
+			mkdir[i] = name[i];					
+		}		
+		if ( strcmp(mkdir, "mkdir ") == 0 && name[6] == '/')
+		{		
+			for(i = 6; i < strlen(name)-1; ++i)
+			{
+				mkdir2[i-6] = name[i];
+			}
+			//mkdir(mkdir2);
+			printf("ChamaMkdir\n");			
+			printf("%s", mkdir2);			
+        	}
+
+	}
+	else if(name[0] == 'r')
+	{	
+		char read[5];
+		char read2[4000];
+		int i;	
+		for(i=0; i < 5; ++i)
+		{
+			read[i] = name[i];					
+		}		
+		if ( strcmp(read, "read ") == 0 && name[5] == '/')
+		{		
+			for(i = 5; i < strlen(name)-1; ++i)
+			{
+				read2[i-5] = name[i];
+			}
+			//read(read2);
+			printf("ChamaRead\n");			
+			printf("%s", read2);			
+        	}
+
+	}
+	else if(name[0] == 'u')
+	{	
+		char unlink[7];
+		char unlink2[4000];
+		int i;	
+		for(i=0; i < 7; ++i)
+		{
+			unlink[i] = name[i];					
+		}		
+		if ( strcmp(unlink, "unlink ") == 0 && name[7] == '/')
+		{		
+			for(i = 7; i < strlen(name)-1; ++i)
+			{
+				unlink2[i-7] = name[i];
+			}
+			//unlink(unlink2);	
+			printf("ChamaUnlink\n");			
+			printf("%s", unlink2);		
+        	}
+
+	}
+	else if(name[0] == 'w')
+	{	
+		char write[6];
+		char write2[4000];
+		int i;	
+		for(i=0; i < 6; ++i)
+		{
+			write[i] = name[i];					
+		}		
+		if ( strcmp(write, "write ") == 0 && name[6] == '/')
+		{		
+			for(i = 6; i < strlen(name)-1; ++i)
+			{
+				write2[i-6] = name[i];
+			}
+			//write(write2);	
+			printf("ChamaWrite\n");			
+			printf("%s", write2);		
+        	}
+
+	}
+	
 	/*load();*/
 	/*char path[] = "/usr";*/
 	/*char* dir = strtok(path, "/");*/
@@ -232,8 +427,9 @@ int main(void)
 	/*printf("get_name(\"%s\") -> %s\n", path ,(char*)get_name(path));*/
 	/*printf("get_name(\"%s\") -> %s\n", path2 ,(char*)get_name(path2));*/
 
-	char* path  = "/usr";
-	mkdir(path);
+	//char* path  = "/usr";
+	//mkdir(path);
+
 
 	path = "/bin";
 	mkdir(path);
@@ -250,5 +446,7 @@ int main(void)
 	/*char* path2 = "/usr/bin/7z";*/
 	/*mkdir(path2);*/
 	/*mkdir("/");*/
-	return 0;
+
+	return 0;	
+	
 }
